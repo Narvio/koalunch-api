6996407f0f15659a6f652d7b76258881e2fd98b8
Michal Kozubik <michal.kozubik@sap.com>
2022-09-18 23:37:23 +0200

    add Goa Slatina

---
+++ KoalunchApi/MenuParsers/WebScrapers/GoaSlatina.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using koalunch_api.Models.Api;
+
+namespace koalunch_api.MenuParsers
+{
+    public class GoaSlatina : Parser
+    {
+        private int CurrentDayIndex
+        {
+            get => 2;
+        }
+
+        public async Task<MenuSection[]> ParseDay(IDocument document)
+        {
+            var todayDataStart = document.QuerySelectorAll("table tr h2")[CurrentDayIndex];
+
+
+            return await Task.FromResult(new MenuSection[] {
+                new MenuSection
+                {
+                    meals = ProcessMenuSection(todayDataStart)
+                }
+            });
+        }
+
+        private Meal[] ProcessMenuSection(IElement dayTitleElement)
+        {
+            var current = dayTitleElement.NextSibling;
+            var meals = new List<Meal> { };
+
+
+            while (current != null && current.NodeName != "H2" && current.NodeName != "P")
+            {
+                if (current.NodeName == "#text") {
+                    var price = System.Text.RegularExpressions.Regex.Match(current.TextContent, "\\d+Kč");
+                    var name = current.TextContent.Remove(price.Index, price.Length);
+                    
+                    meals.Add(new Meal
+                    {
+                        name = Normalize(name),
+                        price = Normalize(price.Value)
+                    });
+                }
+                current = current.NextSibling;
+            }
+
+            return meals.ToArray();
+        }
+
+        private string Normalize(string input)
+        {
+            return input.Trim();
+        }
+    }
+}
